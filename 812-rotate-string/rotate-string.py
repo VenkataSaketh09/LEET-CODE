8a94db5b08736b1e0b377cbf0fcc3d84253d366a
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-05-27 05:31:46 +0530

    Time: 28 ms (91.09%) | Memory: 16.3 MB (99.91%) - LeetSync

---
+++ 812-rotate-string/rotate-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s)!=len(goal):
+            return False
+        temp=s+s
+        if goal in temp:
+            return True
+        return False
+        
