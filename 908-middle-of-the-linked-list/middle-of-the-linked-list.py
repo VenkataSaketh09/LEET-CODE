238b18254cda643fee394bfecf7fa7855a81c577
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-06-01 06:54:53 +0530

    Time: 39 ms (25.44%) | Memory: 16.4 MB (98.06%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp=head
+        count=0
+        while temp:
+            temp=temp.next
+            count+=1
+        mid=count//2
+        temp=head
+        for _ in range(mid):
+            temp=temp.next
+        return temp
+        
+            
+
+        
+
+        
