085db202023240aceabdd6491b9072859e615234
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-06-22 05:16:57 +0530

    Time: 28 ms (94.42%) | Memory: 16.5 MB (63.36%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,11 +1,18 @@
 class Solution:
     def fib(self, n: int) -> int:
-        a=0
-        b=1
-        if n==0:
-            return a
-        elif n==1:
-            return b
-        else:
-            return self.fib(n-1)+self.fib(n-2)
+        # a=0
+        # b=1
+        # if n==0:
+        #     return a
+        # elif n==1:
+        #     return b
+        # else:
+        #     return self.fib(n-1)+self.fib(n-2)
+
+        #Formula
+        sqrt5=5**0.5
+        val1=(1+sqrt5)
+        val2=(1-sqrt5)
+        ans=(val1/2)**n-(val2/2)**n
+        return round(ans/sqrt5)
         
