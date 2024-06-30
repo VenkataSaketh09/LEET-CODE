17b785d1fb30437fec33b4024bd9736d4ed67d88
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-06-30 05:44:43 +0530

    Time: 561 ms (98.59%) | Memory: 50.4 MB (57.55%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -5,22 +5,31 @@
 #         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        count=1
-        temp=head
-        if not head or not head.next:
-            return head.next
-        while temp.next:
-            count+=1
-            temp=temp.next
-        ind=(count//2)
-        if ind==0:
-            return head.next
-        temp=head
-        for _ in range(ind-1):
-            temp=temp.next
-        if temp.next and temp.next.next:
-            temp.next = temp.next.next
-        else:
-            temp.next = None
-        return head
+        # count=1
+        # temp=head
+        # if not head or not head.next:
+        #     return head.next
+        # while temp.next:
+        #     count+=1
+        #     temp=temp.next
+        # ind=(count//2)
+        # if ind==0:
+        #     return head.next
+        # temp=head
+        # for _ in range(ind-1):
+        #     temp=temp.next
+        # if temp.next and temp.next.next:
+        #     temp.next = temp.next.next
+        # else:
+        #     temp.next = None
+        # return head
+        dummy=ListNode(0)
+        dummy.next=head
+        slow=dummy
+        fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        slow.next=slow.next.next
+        return dummy.next
         
