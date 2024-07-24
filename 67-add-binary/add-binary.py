9d2d4ba77252abf568bc10b7ae64895033c1d229
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-07-24 07:17:52 +0530

    Time: 43 ms (21.83%) | Memory: 16.5 MB (82.06%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,18 +1,19 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        ans=[]
-        i=len(a)-1
-        j=len(b)-1
+        res=''
+        n1=len(a)-1
+        n2=len(b)-1
         carry=0
-        while i>=0 or j>=0 or carry:
-            if i>=0:
-                carry+=int(a[i])
-                i-=1
-            if j>=0:
-                carry+=int(b[j])
-                j-=1
-            ans.append(str(carry%2))
+        while n1>=0 or n2>=0 or carry!=0:
+            if n1>=0:
+                carry+=int(a[n1])
+                n1-=1
+            if n2>=0:
+                carry+=int(b[n2])
+                n2-=1
+            res+=str(carry%2)
             carry//=2
-        return ''.join(reversed(ans))
+        return res[::-1]
+
 
         
