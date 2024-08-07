952733f0f17748b108865798511d25d503857cf8
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-08-07 05:51:33 +0530

    Time: 42 ms (41.54%) | Memory: 16.6 MB (46.10%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,19 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        array1_ind=len(nums1)-1
+        arr1_len=len(nums1)-1
         while m>0 and n>0:
-            if nums2[n-1] >= nums1[m-1]:
-                nums1[array1_ind]=nums2[n-1]
-                n-=1
-            else:
-                nums1[array1_ind]=nums1[m-1]
-                m-=1
-            array1_ind-=1
-        while n>0:
-            nums1[array1_ind]=nums2[n-1]
+          if nums2[n-1]>=nums1[m-1]:
+            nums1[arr1_len]=nums2[n-1]
             n-=1
-            array1_ind-=1
+          else:
+            nums1[arr1_len]=nums1[m-1]
+            m-=1
+          arr1_len-=1
+        while n>0:
+          nums1[arr1_len]=nums2[n-1]
+          n-=1
+          arr1_len-=1
+        
 
 
         
