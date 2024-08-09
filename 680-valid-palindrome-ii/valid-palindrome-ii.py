501480cbda917b5c396db1dffdab1aa9481788b5
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-08-09 05:45:37 +0530

    Time: 73 ms (87.96%) | Memory: 16.9 MB (87.67%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -3,12 +3,11 @@ class Solution:
         ptr1=0
         ptr2=len(s)-1
         while ptr1<=ptr2:
-            if s[ptr1]!=s[ptr2]:
-                st1=s[:ptr1]+s[ptr1+1:]
-                st2=s[:ptr2]+s[ptr2+1:]
-                return st1==st1[::-1] or st2==st2[::-1]
-            ptr1+=1
-            ptr2-=1
+          if s[ptr1]!=s[ptr2]:
+            string1=s[:ptr1]+s[ptr1+1:]
+            string2=s[:ptr2]+s[ptr2+1:]
+            return (string1 == string1[::-1]) or (string2 == string2[::-1])
+          ptr1+=1
+          ptr2-=1
         return True
-
         
