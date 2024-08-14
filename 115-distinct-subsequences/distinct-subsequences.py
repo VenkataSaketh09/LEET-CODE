cb30c313aaf81c9d773d8d0e3f3aac0aca16608b
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-08-14 08:08:14 +0530

    Time: 304 ms (54.74%) | Memory: 73.7 MB (36.19%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+      #dp approach (Tabulation)
+      n=len(s)
+      m=len(t)
+      dp=[[0 for i in range(m+1)]for j in range(n+1)]
+      #base cases
+      for i in range(n+1):
+        dp[i][0]=1
+      for i in range(1,m+1):  
+        #to avoid rewriting we avoid rewriting.we consider the loop iteration from 1.
+        dp[0][i]=0
+      
+      for i in range(1,n+1):
+        for j in range(1,m+1):
+          if s[i-1]==t[j-1]:
+            dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+          else:
+            dp[i][j]=dp[i-1][j]
+      return dp[n][m]
+
+      
+        
