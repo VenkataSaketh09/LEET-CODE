b38c9fe113aa6cf0de7d1597f2b25c080f426cff
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-08-23 16:30:28 +0530

    Time: 80 ms (26.65%) | Memory: 17.9 MB (39.38%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,10 +1,45 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        ind1,ind2=-1,-1
-        for i in range(len(nums)):
-            if nums[i]==target:
-                if ind1==-1:
-                    ind1=i
-                ind2=i
-        return [ind1,ind2]
-        
+        # ind1,ind2=-1,-1
+        # for i in range(len(nums)):
+        #     if nums[i]==target:
+        #         if ind1==-1:
+        #             ind1=i
+        #         ind2=i
+        # return [ind1,ind2]
+        def finding_first(arr,target):
+          low=0
+          high=len(arr)-1
+          first=-1
+          while low<=high:
+            mid=(low+high)//2
+            if arr[mid]==target:
+              first=mid
+              high=mid-1
+            elif arr[mid]<target:
+              low=mid+1
+            else:
+              high=mid-1
+          return first
+        def finding_last(arr,target):
+          low=0
+          high=len(arr)-1
+          last=-1
+          while low<=high:
+            mid=(low+high)//2
+            if arr[mid]==target:
+              last=mid
+              low=mid+1
+            elif arr[mid]<target:
+              low=mid+1
+            else:
+              high=mid-1
+          return last
+        def returning(arr,target):
+          first=finding_first(arr,target)
+          last=finding_last(arr,target)
+          if first==-1:
+            return [-1,-1]
+          return [first,last]
+        ans=returning(nums,target)
+        return ans
