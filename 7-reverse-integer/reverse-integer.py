0e659f20cea30be18369e2450565b19ed2da55b8
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-11-06 06:19:34 +0530

    Time: 27 ms (96.26%) | Memory: 16.6 MB (19.26%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,9 +1,21 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x>=0:
-            ans=int(str(x)[::-1])
-        else:
-            ans=int('-'+str(abs(x))[::-1])
-        return ans if  -2**31<=ans<=(2**31)-1 else 0
+        # if x>=0:
+        #     ans=int(str(x)[::-1])
+        # else:
+        #     ans=int('-'+str(abs(x))[::-1])
+        # return ans if  -2**31<=ans<=(2**31)-1 else 0
+        temp=abs(x)
+        rev=0
+        while temp!=0:
+            digit=temp%10
+            rev=rev*10+digit
+            temp//=10
+        if -2**31<=rev<=2**31-1:
+            if x<0:
+                return -rev
+            return rev
+        return 0
+
         
         
