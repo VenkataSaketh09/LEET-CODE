8caf6f1a88f22f653d5adac18507c8a26aaf6981
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-11-07 20:18:37 +0530

    Time: 3 ms (69.01%) | Memory: 18.7 MB (89.32%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,11 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        if len(nums)==1:
-            return nums[0]
-        di={}
-        for i in nums:
-            di[i]=di.get(i,0)+1
-        for i in di:
-            if di[i]==1:
-                return i
+        # if len(nums)==1:
+        #     return nums[0]
+        # di={}
+        # for i in nums:
+        #     di[i]=di.get(i,0)+1
+        # for i in di:
+        #     if di[i]==1:
+        #         return i
         
+        #using the xor is optimal approach
+        xorr=0
+        for i in nums:
+            xorr^=i
+
+        return xorr
