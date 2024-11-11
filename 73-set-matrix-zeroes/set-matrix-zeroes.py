06b2605506467e6f4cd7d2dc0d56bbaa6dcdb3ff
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-11-11 08:53:17 +0530

    Time: 4 ms (42.47%) | Memory: 17.4 MB (54.90%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -8,17 +8,15 @@ class Solution:
         row_data=set()
         col_data=set()
         for i in range(rows):
-          for j in range(cols):
-            if matrix[i][j]==0:
-              row_data.add(i)
-              col_data.add(j)
+            for j in range(cols):
+                if matrix[i][j]==0:
+                    row_data.add(i)
+                    col_data.add(j)
         for i in row_data:
-          for j in range(cols):
-            matrix[i][j]=0
-        
+            for j in range(cols):
+                matrix[i][j]=0
         for i in col_data:
-          for j in range(rows):
-            matrix[j][i]=0
-        
+            for j in range(rows):
+                matrix[j][i]=0
 
         
