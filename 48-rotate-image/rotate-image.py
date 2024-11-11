e1c622ff6b7c47e93b8dedc36a6c1830731ecc4e
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-11-11 09:04:01 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (37.89%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -5,10 +5,11 @@ class Solution:
         """
         n=len(matrix)
         for i in range(n):
-          for j in range(i,n):
-            matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+            for j in range(i,n):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
         for i in range(n):
-          matrix[i].reverse()
+            matrix[i].reverse()
+            
 
           
 
