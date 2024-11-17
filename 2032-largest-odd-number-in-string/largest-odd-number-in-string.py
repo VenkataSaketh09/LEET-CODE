5d804da0c50319d58f15b4da81cd66f7edad729a
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-11-17 05:38:40 +0530

    Time: 7 ms (90.43%) | Memory: 17.8 MB (71.16%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -1,4 +1,10 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
-        return num[:max(num.rfind(x) for x in '13579')+1] 
-        
+        # return num[:max(num.rfind(x) for x in '13579')+1] 
+        n=len(num)
+        result=n-1
+        while result>=0 and int(num[result])%2==0:
+            result-=1
+        if result<0:
+            return ""
+        return num[:result+1]
