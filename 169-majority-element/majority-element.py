1a7e88987ad83f194b1563d2bd542d35c4205f67
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-12-09 10:43:37 +0530

    Time: 15 ms (18.45%) | Memory: 18.8 MB (11.66%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-      n=len(nums)//2
-      di={}
-      for i in nums:
-        di[i]=di.get(i,0)+1
-      for key,val in di.items():
-        if val>n:
-          return key
-      
+        di={}
+        ans=0
+        more_repeat=0
+        for i in nums:
+            di[i]=di.get(i,0)+1
+            if di[i]>more_repeat:
+                ans=i
+                more_repeat=di[i]
+        return ans
         
