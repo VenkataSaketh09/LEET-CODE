2f0f8815544cf740a6e7ba70149ef6ba444ee272
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-12-09 21:26:27 +0530

    Time: 76 ms (20.11%) | Memory: 42.8 MB (20.74%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -1,15 +1,24 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        pos_arr=[]
-        neg_arr=[]
+        pos=[]
+        neg=[]
+        res=[]
         for i in nums:
-            if i>=0:
-                pos_arr.append(i)
+            if i>0:
+                pos.append(i)
             else:
-                neg_arr.append(i)
-        modified_arr=[]
-        for i,j in zip(pos_arr,neg_arr):
-            modified_arr.append(i)
-            modified_arr.append(j)
-        return modified_arr    
+                neg.append(i)
+        ptr1=ptr2=0
+        while ptr1<len(pos) and ptr2<len(neg):
+            res.append(pos[ptr1])
+            res.append(neg[ptr2])
+            ptr1+=1
+            ptr2+=1
+        while ptr1<len(pos):
+            res.append(pos[ptr1])
+            ptr1+=1
+        while ptr2<len(neg):
+            res.append(neg[ptr2])
+            ptr2+=1
+        return res
         
