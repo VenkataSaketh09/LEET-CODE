68ac761b8d1b5289dc6b98f04845a85bce07373a
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2024-12-13 06:52:52 +0530

    Time: 574 ms (61.76%) | Memory: 20.2 MB (36.47%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,29 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-      #Two-Pointer Approach
-      nums.sort()
-      ans=[]
-      for i in range(len(nums)):
-        if i!=0 and nums[i]==nums[i-1]:
-          continue
-        
-        j=i+1
-        k=len(nums)-1
-        while j<k:
-          val=nums[i]+nums[j]+nums[k]
-          if val>0:
-            k-=1
-          elif val<0:
-            j+=1
-          else:
-            ans.append([nums[i], nums[j], nums[k]])  
-            k-=1
-            while j<k and nums[j]==nums[j-1]:
-              j+=1
-            while j<k and nums[k]==nums[k+1]:
-              k-=1
-      return ans
-            
-
-
+        #optimal Approach
+        res=[]
+        nums.sort()
+        n=len(nums)
+        for i in range(n):
+            if i!=0 and nums[i]==nums[i-1]:
+                continue
+            j=i+1
+            k=n-1
+            while j<k:
+                Sum=nums[i]+nums[j]+nums[k]
+                if Sum<0:
+                    j+=1
+                elif Sum>0:
+                    k-=1
+                else:
+                    temp=[nums[i],nums[j],nums[k]]
+                    res.append(temp)
+                    j+=1
+                    k-=1
+                    while j<k and nums[j]==nums[j-1]:
+                        j+=1
+                    while j<k and nums[k]==nums[k+1]:
+                        k-=1
+        return res
+                
         
