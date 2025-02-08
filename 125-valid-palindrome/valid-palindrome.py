85648a1352e68440440f48ba81373792bd9d2403
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2025-02-08 08:27:06 +0530

    Time: 7 ms (81.13%) | Memory: 17.9 MB (77.75%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,16 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        left=0
-        right=len(s)-1
-        while left<right:
-            if not s[left].isalnum():
-                left+=1
-            elif not s[right].isalnum():
-                right-=1
-            elif s[left].casefold()==s[right].casefold():
-                left+=1
-                right-=1
-            else:
+        l=0
+        r=len(s)-1
+        while l<r:
+            if not s[l].isalnum():
+                l+=1
+            elif not s[r].isalnum():
+                r-=1
+            elif s[l].lower()!=s[r].lower():
                 return False
+            else:
+                l+=1
+                r-=1
         return True
         
