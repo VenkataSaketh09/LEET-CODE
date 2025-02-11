d83c73ea3e7325f336c3a13787e3c1ce360546b6
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2025-02-11 15:03:19 +0530

    Time: 15 ms (16.82%) | Memory: 19.4 MB (18.57%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,12 +1,29 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        di={}
-        ans=0
-        more_repeat=0
-        for i in nums:
-            di[i]=di.get(i,0)+1
-            if di[i]>more_repeat:
-                ans=i
-                more_repeat=di[i]
-        return ans
+
+        #Boyeer-Moores Voting Algorithm
+        #it has teo phases:1.candidate selection 2.verification
+
+        #phase1 :
+        count=0
+        ele=None
+        n=len(nums)
+        for i in range(n):
+            if count==0:
+                count=1
+                ele=nums[i]
+            elif ele==nums[i]:
+                count+=1
+            else:
+                count-=1
+        #phase2:
+        count=0
+        for i in range(n):
+            if nums[i]==ele:
+                count+=1
+        if count>n//2:
+            return ele
+        return -1
+        
+
         
