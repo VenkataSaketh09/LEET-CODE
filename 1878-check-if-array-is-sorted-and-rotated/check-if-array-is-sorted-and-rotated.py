6d6e49dc3fa8ea760149678bb77464f68b758528
MUDDU VENKATA SAKETH <22pa1a12b4@vishnu.edu.in>
2025-02-16 15:46:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.99%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -1,14 +1,16 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        ptr=0
+        pointer=0
         for i in range(len(nums)-1):
             if nums[i]>nums[i+1]:
                 break
-            ptr+=1
-        temp=nums[ptr+1:]+nums[:ptr+1]
+            pointer+=1
+        nums=nums[pointer+1:]+nums[:pointer+1]
         for i in range(len(nums)-1):
-            if temp[i]>temp[i+1]:
+            if nums[i]>nums[i+1]:
                 return False
         return True
+    
 
-        
+
+    
